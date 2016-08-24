b936f859bb3f5257c9e005c3623a7ad7dd16a278
Matthew Peychich and Rogelio J. Samour <dev+mpeychich+therubymug@goldstar.com>
2016-08-24 13:52:42 -0700

    Tweak git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -179,11 +179,11 @@ merge() {
   git branch -D ${branch}
 }
 
-alias gap='git add -p'
-alias gc='git commit -v'
-alias gd='git diff --no-ext-diff'
-alias gdc='gds'
-alias gds='git diff --staged --no-ext-diff'
-alias gnap='git add -N . && git add -p'
-alias gpr='git pull --rebase --prune'
-alias gst='git status'
+alias gap="git add -p"
+alias gc="git commit -vS"
+alias gd="git diff --no-ext-diff"
+alias gdc="gds"
+alias gds="git diff --staged --no-ext-diff"
+alias gnap="git add -N . && git add -p"
+alias gpr="git pull --rebase --prune --jobs=$(sysctl -n hw.ncpu)"
+alias gst="git status"
